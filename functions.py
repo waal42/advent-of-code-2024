8c9d64e0f31ee31b4043235752257ee26494c4a0
waal42 <filipvalasek@gmail.com>
2024-11-27 01:15:24 +0100

    fancy input tools a template z dřívějška

---
+++ functions.py
@@ -0,0 +1,48 @@
+import functools
+from time import perf_counter
+
+
+def timer(func):
+    """- courtesy of filakrad"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):        
+        start_time = perf_counter()
+        value = func(*args, **kwargs)
+        end_time = perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time} seconds.")
+        return value
+    return wrapper_timer
+
+
+def comma_separated_line(filename):
+    with open(filename, "r", encoding="utf-8") as file_in:
+        return list(file_in.read().strip("\n").split(", "))
+
+
+def lines(filename):
+    with open(filename, "r", encoding="utf-8") as file_in:
+        return list(file_in.read().split("\n"))
+    
+def lines_of_numbers(filename, separator):
+    with open(filename, "r", encoding="utf-8") as file_in:
+        return list([[int(y) for y in x.split(separator)] for x in file_in.read().split("\n")])
+
+
+def blocks_of_lines(filename):
+    with open(filename, "r", encoding="utf-8") as file_in:
+        return [line.split("\n") for line in file_in.read().split("\n\n")]
+
+
+def tuples(filename):
+    with open(filename, "r", encoding="utf-8") as file_in:
+        return [tuple.split() for tuple in file_in.read().split("\n")]
+
+
+def comma_separated_ranges(filename):
+    with open(filename, "r", encoding="utf-8") as file_in:
+        data_out = list()
+        for tuple in file_in.read().split("\n"):
+            data_out.append([list(map(int, (range.split("-"))))
+                            for range in tuple.split(",")])
+        return data_out
