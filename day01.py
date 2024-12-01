2bb49389815377bdd654cdc25aac745945bc0d59
waal42 <filipvalasek@gmail.com>
2024-12-01 23:56:31 +0100

    první den patří nám

--- day01.py
+++ day01.py
@@ -14,13 +14,22 @@ def star_one(data_in):
 @timer
 def star_two(data_in):
     right_column = dict()
-    for 
+    for number in data_in[1]:
+        if number in right_column.keys():
+            right_column[number] += 1
+        else:
+            right_column[number] = 1
+    total = 0
+    for number in data_in[0]:
+        if number in right_column.keys():
+            total += number * right_column[number]
+    return total
 
 
 @timer
 def main():
     # data = []
-    data = load_data("day01testinput.txt")
+    data = load_data("day01input.txt")
     # print(data)
     print(star_one(data))
     print(star_two(data))
